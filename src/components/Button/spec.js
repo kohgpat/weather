c31446011da729aaaf85d0a410ec10fe1d095d81
Nikolay Burlov <kohgpat@gmail.com>
2018-02-07 03:46:53 +0800

    Add snapshot tests for Button, Input and Container

---
+++ src/components/Button/spec.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Button from "./index";
+import { shallow } from "enzyme";
+
+it("renders", () => {
+  const button = shallow(<Button>Button</Button>);
+  expect(button).toMatchSnapshot();
+});
---
+++ src/components/Container/spec.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Container from "./index";
+import { shallow } from "enzyme";
+
+it("renders", () => {
+  const container = shallow(<Container />);
+  expect(container).toMatchSnapshot();
+});
