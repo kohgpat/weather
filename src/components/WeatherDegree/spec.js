e0c702fc8e0185dc08848724c8bc35271b05a6b8
Nikolay Burlov <kohgpat@gmail.com>
2018-02-09 21:14:58 +0800

    Add additinal tests to WeatherDegree

--- src/components/WeatherDegree/spec.js
+++ src/components/WeatherDegree/spec.js
@@ -1,6 +1,6 @@
 import React from "react";
 import WeatherDegree from "./index";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 
 it("renders null", () => {
   const weatherDegree = shallow(<WeatherDegree units="" />);
@@ -15,4 +15,19 @@ it("renders Celcius", () => {
 it("renders Fahrenheit", () => {
   const weatherDegree = shallow(<WeatherDegree units="fahrenheit" />);
   expect(weatherDegree).toMatchSnapshot();
+});
+
+it("returns null", () => {
+  const weatherDegree = shallow(<WeatherDegree units="" />);
+  expect(weatherDegree.type()).toEqual(null);
+});
+
+it("renders Celcius symbol", () => {
+  const weatherDegree = mount(<WeatherDegree units="celcius" />);
+  expect(weatherDegree.text()).toContain("C");
+});
+
+it("renders Fahrenheit symbol", () => {
+  const weatherDegree = mount(<WeatherDegree units="fahrenheit" />);
+  expect(weatherDegree.text()).toContain("F");
 });
