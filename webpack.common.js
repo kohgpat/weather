4635e5638815cc19bfc57e35b98f6ed120320502
Nikolay Burlov <kohgpat@gmail.com>
2019-05-13 01:00:00 +0800

    Fix now build and change build to dist for deployment

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ const WriteFilePlugin = require("write-file-webpack-plugin");
 module.exports = {
   entry: ["@babel/polyfill", "./src/index.js"],
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
   },
   module: {
